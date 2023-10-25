9d14428b5276c433bb80a25d0622f4ca199c0622
lelemathrin <leo.mathurin@epitech.digital>
2023-10-25 17:01:09 +0200

    added characters descriptions

--- src/main.py
+++ src/main.py
@@ -15,11 +15,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 client = discord.Client(intents=intents)
 
 class Personnage:
-    def __init__(self, nom, metier, status, hierarchy):
+    def __init__(self, nom, metier, status, hierarchy, description):
         self.nom = nom
         self.metier = metier
         self.status = status
         self.hierarchy = hierarchy
+        self.description = description  # Nouvelle propriété pour la description
 
     def introduce_hisself(self):
         return f"Bonjour, je m'appelle {self.nom} et je suis le {self.metier}."
@@ -31,12 +32,12 @@ class Personnage:
         return f"Je suis {self.nom}."
 
 channels_to_personnages = {
-    "commissariat": Personnage("David", "détective", True, True),
-    "Archibald Obscurus": Personnage("Monsieur Archibald Obscurus", "???", True, False),
-    "Scarlett Sombreval": Personnage("Mademoiselle Scarlett Sombreval", "???", True, False),
-    "Orion Sangfroid": Personnage("Orion Sangfroid", "???", True, False),
-    "Casyope Lycanthrope": Personnage("Casyope Lycanthrope", "???", True, False),
-    "Edgar Frisson": Personnage("Monsieur Edgar Frisson", "???", True, False)
+    "commissariat": Personnage("David", "détective", True, True, ""),
+    "Archibald Obscurus": Personnage("Monsieur Archibald Obscurus", "fantôme, boulanger", True, False, "Un érudit calme et énigmatique, obsédé par l'étude des phénomènes surnaturels. Il parle rarement de lui-même, préférant discuter de sujets ésotériques avec ceux qui peuvent le suivre dans ses intérêts."),
+    "Scarlett Sombreval": Personnage("Mademoiselle Scarlett Sombreval", "sorcière, infirmière", True, False, "Une actrice théâtrale charismatique avec une passion pour le macabre. Elle est extravertie, aime être au centre de l'attention et a une collection de souvenirs de films d'horreur dans sa chambre."),
+    "Orion Sangfroid": Personnage("Orion Sangfroid", "diable, fleuriste", True, False, "Un homme stoïque avec une fascination pour les fleurs. Il parle peu de ses propres expériences, mais son regard intense suggère une vie pleine de mystères."),
+    "Casyope Lycanthrope": Personnage("Casyope Lycanthrope", "loup-garou maire fan de science", True, False, "Un maire et scientifique déterminé et curieux, passionné par l'étude des créatures surnaturelles. Il est pragmatique et sceptique, mais toujours ouvert à la découverte de nouvelles vérités."),
+    "Edgar Frisson": Personnage("Monsieur Edgar Frisson", "zombie, facteur", True, False, "Il est discret, mais il semble avoir un sixième sens pour les mystères. Sa présence discrète cache peut-être plus qu'il ne le laisse paraître.")
 }
 
 async def chat_with_gpt(message, personnage):
@@ -101,18 +102,23 @@ async def on_message(message):
 
         # Le commissaire a toujours son propre canal
         commissaire_channel = await guild.create_text_channel("commissariat", category=category)
-        await commissaire_channel.send(f'Bienvenue dans commissariat !')
+        await commissaire_channel.send(f'Bienvenue dans le canal du commissariat !\nMétier: {channels_to_personnages["commissariat"].metier}')
 
         # Les noms des autres personnages
         other_character_names = ["Archibald Obscurus", "Scarlett Sombreval", "Orion Sangfroid", "Casyope Lycanthrope", "Edgar Frisson"]
         random.shuffle(other_character_names)  # Mélange les noms aléatoirement
 
         # Crée un canal pour chaque personnage
-        for name in other_character_names[:6]:  # Prend les 6 premiers noms après mélange
-            channel = await guild.create_text_channel(name.replace(" ", "_").lower(), category=category)
-            await channel.send(f'Bienvenue dans {name} !')
-
-        await message.channel.send(f'Canaux créés dans la catégorie "{category_name}"')
+        for name in other_character_names[:6]:  
+            channel_name = name.replace(" ", "_").lower()
+            channel = await guild.create_text_channel(channel_name, category=category)
+            personnage = channels_to_personnages[name]
+            
+            # Si le personnage a une description, envoyez-la.
+            if personnage.description:
+                await channel.send(f'Bienvenue dans le canal de {personnage.nom} !\nMétier: {personnage.metier}\nDescription: {personnage.description}')
+            else:
+                await channel.send(f'Bienvenue dans le canal de {personnage.nom} !\nMétier: {personnage.metier}')
 
 
     if message.content.startswith('/end'):
