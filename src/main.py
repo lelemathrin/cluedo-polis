f2885029f42b0ed44f4891c0294e99883c6b88c4
lelemathrin <leo.mathurin@epitech.digital>
2023-10-26 00:52:21 +0200

    limited questions, names and images of characters

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import openai
 import random
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,11 +16,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 client = discord.Client(intents=intents)
 
 class Personnage:
-    def __init__(self, nom, metier, status, hierarchy, description):
+    def __init__(self, nom, metier, status, hierarchy, image_path, description):
         self.nom = nom
         self.metier = metier
         self.status = status
         self.hierarchy = hierarchy
+        self.image_path = image_path
         self.description = description  # Nouvelle propriété pour la description
 
     def introduce_hisself(self):
@@ -32,13 +34,15 @@ class Personnage:
         return f"Je suis {self.nom}."
 
 channels_to_personnages = {
-    "commissariat": Personnage("David", "détective", True, True, ""),
-    "Archibald Obscurus": Personnage("Monsieur Archibald Obscurus", "fantôme, boulanger", True, False, "Un érudit calme et énigmatique, obsédé par l'étude des phénomènes surnaturels. Il parle rarement de lui-même, préférant discuter de sujets ésotériques avec ceux qui peuvent le suivre dans ses intérêts."),
-    "Scarlett Sombreval": Personnage("Mademoiselle Scarlett Sombreval", "sorcière, infirmière", True, False, "Une actrice théâtrale charismatique avec une passion pour le macabre. Elle est extravertie, aime être au centre de l'attention et a une collection de souvenirs de films d'horreur dans sa chambre."),
-    "Orion Sangfroid": Personnage("Orion Sangfroid", "diable, fleuriste", True, False, "Un homme stoïque avec une fascination pour les fleurs. Il parle peu de ses propres expériences, mais son regard intense suggère une vie pleine de mystères."),
-    "Casyope Lycanthrope": Personnage("Casyope Lycanthrope", "loup-garou maire fan de science", True, False, "Un maire et scientifique déterminé et curieux, passionné par l'étude des créatures surnaturelles. Il est pragmatique et sceptique, mais toujours ouvert à la découverte de nouvelles vérités."),
-    "Edgar Frisson": Personnage("Monsieur Edgar Frisson", "zombie, facteur", True, False, "Il est discret, mais il semble avoir un sixième sens pour les mystères. Sa présence discrète cache peut-être plus qu'il ne le laisse paraître.")
+    "commissariat": Personnage("David", "détective", True, True, "assets/11minutos.jpg", ""),
+    "archibald_obscurus": Personnage("Monsieur Archibald Obscurus", "fantôme, boulanger", True, False, "assets/11minutos.jpg", "Un érudit calme et énigmatique, obsédé par l'étude des phénomènes surnaturels. Il parle rarement de lui-même, préférant discuter de sujets ésotériques avec ceux qui peuvent le suivre dans ses intérêts."),
+    "scarlett_sombreval": Personnage("Mademoiselle Scarlett Sombreval", "sorcière, infirmière", True, False, "assets/11minutos.jpg", "Une actrice théâtrale charismatique avec une passion pour le macabre. Elle est extravertie, aime être au centre de l'attention et a une collection de souvenirs de films d'horreur dans sa chambre."),
+    "orion_sangfroid": Personnage("Orion Sangfroid", "diable, fleuriste", True, False, "assets/11minutos.jpg", "Un homme stoïque avec une fascination pour les fleurs. Il parle peu de ses propres expériences, mais son regard intense suggère une vie pleine de mystères."),
+    "casyope_lycanthrope": Personnage("Casyope Lycanthrope", "loup-garou maire fan de science", True, False, "assets/lhommeleplusaigridefrance.jpg", "Un maire et scientifique déterminé et curieux, passionné par l'étude des créatures surnaturelles. Il est pragmatique et sceptique, mais toujours ouvert à la découverte de nouvelles vérités."),
+    "edgar_frisson": Personnage("Monsieur Edgar Frisson", "zombie, facteur", True, False, "assets/lhommeleplusaigridefrance.jpg", "Il est discret, mais il semble avoir un sixième sens pour les mystères. Sa présence discrète cache peut-être plus qu'il ne le laisse paraître.")
 }
+
+user_message_count = {}  # Format : {user_id: {channel_name: count}}
 
 async def chat_with_gpt(message, personnage):
     user_message = message.content
@@ -53,33 +57,20 @@ async def chat_with_gpt(message, personnage):
     except Exception as e:
         return str(e)
 
+async def reset_daily_counts():
+    while True:
+        await asyncio.sleep(86400)  # Attendre 24 heures
+        user_message_count.clear()
+        
 @client.event
 async def on_ready():
+    client.loop.create_task(reset_daily_counts())
     print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith('/parler'):
-        await message.channel.send(f'Dans le serveur : {message.guild.name}, avec qui voulez-vous parler : Alice, Bob, Carole ou David ?')
-
-        def check(m):
-            return m.author == message.author and m.channel == message.channel
-
-        response = await client.wait_for('message', check=check)
-
-        personnage = None
-        for nom, pers in channels_to_personnages.items():
-            if nom.lower() in response.content.lower():
-                personnage = pers
-                break
-
-        if personnage:
-            await message.channel.send(personnage.introduce_hisself())
-        else:
-            await message.channel.send("Personnage non trouvé.")
 
     if message.content.startswith('/hello'):
         await message.channel.send('Hello!')
@@ -103,6 +94,8 @@ async def on_message(message):
         # Le commissaire a toujours son propre canal
         commissaire_channel = await guild.create_text_channel("commissariat", category=category)
         await commissaire_channel.send(f'Bienvenue dans le canal du commissariat !\nMétier: {channels_to_personnages["commissariat"].metier}')
+        await commissaire_channel.send(file=discord.File(channels_to_personnages["commissariat"].image_path))
+
 
         # Les noms des autres personnages
         other_character_names = ["Archibald Obscurus", "Scarlett Sombreval", "Orion Sangfroid", "Casyope Lycanthrope", "Edgar Frisson"]
@@ -112,14 +105,15 @@ async def on_message(message):
         for name in other_character_names[:6]:  
             channel_name = name.replace(" ", "_").lower()
             channel = await guild.create_text_channel(channel_name, category=category)
-            personnage = channels_to_personnages[name]
+            personnage = channels_to_personnages[channel_name]
             
             # Si le personnage a une description, envoyez-la.
             if personnage.description:
                 await channel.send(f'Bienvenue dans le canal de {personnage.nom} !\nMétier: {personnage.metier}\nDescription: {personnage.description}')
+                await channel.send(file=discord.File(personnage.image_path))
             else:
                 await channel.send(f'Bienvenue dans le canal de {personnage.nom} !\nMétier: {personnage.metier}')
-
+                await channel.send(file=discord.File(personnage.image_path))
 
     if message.content.startswith('/end'):
         user = message.author
@@ -136,11 +130,29 @@ async def on_message(message):
 
     if message.channel.name in channels_to_personnages:
         personnage = channels_to_personnages[message.channel.name]
+        
+        # Vérifier la limite de messages
+        user_id = message.author.id
+        channel_name = message.channel.name
+        if user_id not in user_message_count:
+            user_message_count[user_id] = {}
+        if channel_name not in user_message_count[user_id]:
+            user_message_count[user_id][channel_name] = 0
+
+        # Définir la limite en fonction du canal
+        limit = 1 if channel_name == "commissariat" else 5
+
+        if user_message_count[user_id][channel_name] >= limit:
+            await message.channel.send("Désolé, vous avez atteint votre limite de messages pour aujourd'hui pour ce personnage.")
+            return  # Ne pas répondre
+
+        # Si la limite n'est pas atteinte, augmenter le compteur et continuer
+        user_message_count[user_id][channel_name] += 1
 
         if personnage.status:
             response_text = await chat_with_gpt(message, personnage)
-            await message.channel.send(response_text)
-            
-    
+            formatted_response = f"**{personnage.nom}**: {response_text}"
+            await message.channel.send(formatted_response)
+
 
 client.run(token)
